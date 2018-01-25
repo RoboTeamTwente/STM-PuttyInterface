4f8ce60bd574cd5a7500d07ba8a7f5b7aa8a938e
Unknown <leonklute@live.nl>
2018-01-25 13:33:46 +0100

    actual gitignore

---
+++ Debug/Src/PuttyInterface/subdir.mk
@@ -0,0 +1,25 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/PuttyInterface/PuttyInterface.c 
+
+OBJS += \
+./Src/PuttyInterface/PuttyInterface.o 
+
+C_DEPS += \
+./Src/PuttyInterface/PuttyInterface.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/PuttyInterface/%.o: ../Src/PuttyInterface/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -mfloat-abi=soft '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F103xB -I"D:/Documents/STM/ImprovingPuttyInterface/Inc" -I"D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc" -I"D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy" -I"D:/Documents/STM/ImprovingPuttyInterface/Middlewares/ST/STM32_USB_Device_Library/Core/Inc" -I"D:/Documents/STM/ImprovingPuttyInterface/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc" -I"D:/Documents/STM/ImprovingPuttyInterface/Drivers/CMSIS/Device/ST/STM32F1xx/Include" -I"D:/Documents/STM/ImprovingPuttyInterface/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ Debug/Src/main.d
@@ -0,0 +1,115 @@
+Src/main.o: ../Src/main.c \
+ D:/Documents/STM/ImprovingPuttyInterface/Inc/main.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Inc/stm32f1xx_hal_conf.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Inc/main.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Drivers/CMSIS/Include/core_cm3.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Drivers/CMSIS/Include/core_cmInstr.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Drivers/CMSIS/Include/core_cmFunc.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_ll_usb.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd_ex.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Inc/usb_device.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Inc/usbd_conf.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Inc/gpio.h \
+ ../Src/PuttyInterface/PuttyInterface.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Inc/usbd_cdc_if.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc/usbd_cdc.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h \
+ D:/Documents/STM/ImprovingPuttyInterface/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h
+
+D:/Documents/STM/ImprovingPuttyInterface/Inc/main.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Inc/stm32f1xx_hal_conf.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Inc/main.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Drivers/CMSIS/Include/core_cm3.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Drivers/CMSIS/Include/core_cmInstr.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Drivers/CMSIS/Include/core_cmFunc.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_ll_usb.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd_ex.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Inc/usb_device.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Inc/usbd_conf.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Inc/gpio.h:
+
+../Src/PuttyInterface/PuttyInterface.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Inc/usbd_cdc_if.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc/usbd_cdc.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h:
+
+D:/Documents/STM/ImprovingPuttyInterface/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h:
